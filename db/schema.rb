1753208ef5c78ce54aecb9dbee769483e4039894
tmbeer9003 <112091599+tmbeer9003@users.noreply.github.com>
2022-11-11 07:23:04 +0000

    [add]admin/breweries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_11_010931) do
+ActiveRecord::Schema.define(version: 2022_11_11_064513) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -56,6 +56,14 @@ ActiveRecord::Schema.define(version: 2022_11_11_010931) do
   create_table "beer_styles", force: :cascade do |t|
     t.string "name", null: false
     t.integer "category", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "breweries", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.integer "location", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

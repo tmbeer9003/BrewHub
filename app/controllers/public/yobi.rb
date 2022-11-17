f45a6ee9b1c44f66c9302c8152f1452324ca597e
tmbeer9003 <112091599+tmbeer9003@users.noreply.github.com>
2022-11-17 02:34:38 +0000

    [Fix] Cheerした投稿の一覧

--- app/controllers/public/yobi.rb
+++ app/controllers/public/yobi.rb
@@ -7,11 +7,10 @@ class Public::HomesController < ApplicationController
   def timeline
     followings = current_member.followings
     timeline_posts = Post.where(member_id: followings).or(Post.where(member_id: current_member.id))
-    # 投稿一覧をフォローしているユーザーと自分の投稿に絞り込み
+    # フォローしているユーザーと自分の投稿に絞り込み
     post_search = params[:post_search]
     # 投稿検索で受け取った値を代入
     if post_search != nil
-    #検索から遷移したら
       @posts = timeline_posts.where("content like ?", "%#{post_search}%").order(id: :desc)
     else
       @posts = timeline_posts.order(id: :desc)
@@ -19,15 +18,15 @@ class Public::HomesController < ApplicationController
   end
 
   def cheers_list
-    cheers_posts = current_member.cheers_posts
-    # 投稿一覧を自分がCheersしている投稿に絞り込み
+    cheers_list = current_member.cheers
+    # ログインユーザーのCheersを絞り込み
     post_search = params[:post_search]
     # 投稿検索で受け取った値を代入
     if post_search != nil
-    #検索から遷移したら
-      @posts = cheers_posts.where("content like ?", "%#{post_search}%").order(id: :desc)
+      @cheers = cheers_list.where("content like ?", "%#{post_search}%").order(id: :desc)
+      #要修正
     else
-      @posts = cheers_posts.order(id: :desc)
+      @cheers = cheers_list.order(id: :desc)
     end
   end
 
